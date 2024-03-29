c8e0dae5ddda9ebb7e80f2676a1256322d4caae1
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-03-28 23:24:51 -0300

    updates

--- src/epb_simullation.py
+++ src/epb_simullation.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import PlasmaBubbles as pb 
 import datetime as dt
+import base as b 
+import GEO as gg 
 
 
 def sunset_like(
@@ -69,10 +71,10 @@ def last_occurrence(df, col):
 
 def add_zeros(
         df, 
-        delta_hours = 2, 
+        delta_hours = 3, 
         dn = None, 
         lon_start = -60, 
-        duration = 3
+        duration = 4
         ):
     
     if dn is None:
@@ -92,14 +94,63 @@ def add_zeros(
 
 dn = dt.datetime(2013, 12, 24, 20)
 
-df = set_data(dn, hours = 13)
+df = set_data(dn, hours = 10)
 
 df[[-80, -70]] = np.nan
 
 df = df.dropna(axis = 1)
 
-df = add_zeros(df, dn = None, lon_start = -60)
+fig, ax = plt.subplots(
+    figsize = (10, 6),
+    nrows = 2,
+    sharex = True, 
+    sharey = True,
+    dpi = 300
+    )
 
-df = add_zeros(df, dn = None, delta_hours = 3, lon_start = -50)
+plt.subplots_adjust(hspace = 0.1)
+start_hours = 2
+df = add_zeros(df, dn = None, delta_hours = start_hours+ 1, lon_start = -60)
 
-df.plot(subplots = True) 
+df = add_zeros(df, dn = None, delta_hours = start_hours , lon_start = -50)
+
+y = 1.2
+
+longs = [-50, -60]
+
+for i, ax in enumerate(ax.flat):
+    
+    col = longs[i]
+    
+    ax.plot(df[col], lw = 2)
+    
+    dusk = pb.terminator(col, dn, float_fmt = False)
+    
+    ax.axvline(dusk, lw = 2)
+    
+    midnight = gg.local_midnight(dn, col + 5, delta_day = 1)
+    
+    ax.axvline(
+        midnight, 
+        lw = 2,
+        color = 'b',
+        )
+    
+    ax.set(ylim = [-0.1, 1.1], 
+           yticks =[0, 1])
+    
+    ax.text(0.1, 0.8, col, transform = ax.transAxes)
+    
+    if i == 0:
+        ax.text(dusk, y, 'Terminator', 
+                    transform = ax.transData)
+            
+        ax.text(midnight, y, 'midnight', 
+                color = 'b',
+                transform = ax.transData)
+    else:
+        
+        b.format_time_axes(ax)
+
+
+plt.show()
--- src/plot_zonal_drift_mapping.py
+++ src/plot_zonal_drift_mapping.py
@@ -5,6 +5,9 @@ import datetime as dt
 import numpy as np 
 import base as b 
 
+import PlasmaBubbles as pb 
+from tqdm import tqdm 
+
 b.config_labels()
 
 
@@ -15,7 +18,7 @@ def displacement(x0, v0, dt):
 
 
 
-def mappping(year):
+def mappping(time):
     fig, ax = plt.subplots(
         dpi = 300,
         ncols = 1,
@@ -27,11 +30,11 @@ def mappping(year):
     
     plt.subplots_adjust(wspace = 0.1)
     
-    # for i in range(3):
-    gg.map_attrs(ax, year)
-    gg.plot_rectangles_regions(ax, year, delta1 = 3)
+    gg.map_attrs(ax, time.year)
+    gg.plot_rectangles_regions(ax, time.year, delta1 = 3)
     
-    
+    ax.set(title = time.strftime("%H:%M:%S (UT)"))
+
     return fig, ax
 
 
@@ -60,7 +63,12 @@ def ellipse(
     
     return x, y
 
-def plot_ellipse(ax, year = 2014, lon = -60):
+def plot_ellipse(
+        ax, 
+        year = 2014, 
+        lon = -60, 
+        semi_major = 10.0
+        ):
     
    
     eq_lon, eq_lat  = gg.load_equator(
@@ -68,86 +76,126 @@ def plot_ellipse(ax, year = 2014, lon = -60):
     
     i = b.find_closest(eq_lon, lon)
     
-    x, y = ellipse((eq_lon[i], eq_lat[i]))
+    x, y = ellipse(
+        (eq_lon[i], eq_lat[i]), 
+        semi_major = semi_major
+        )
     
     ax.plot(x, y)
    
     ax.fill(x, y, color = 'gray', alpha = 0.5)
     
-def plot_terminator_line(ax, dn):
+def plot_terminator_line(dn):
     
     lon, lat = gg.terminator2(dn, 18)
-    ax.scatter(lon, lat, c = 'k', s = 5)
     
     eq_lon, eq_lat = gg.load_equator(year, values = True)
-
-
     ilon, ilat = gg.intersection(eq_lon, eq_lat, lon, lat)
     
-    ax.scatter(ilon, ilat, marker = 'o', s = 100)
+    ax.scatter(lon, lat, c = 'k', s = 5)
+    
+    ax.scatter(ilon, ilat, c = 'k', s = 5)
 
 
     if len(ilon) > 1 and len(ilat)>1:
         ilon = ilon[0]
         ilat = ilat[0]
         
-    return ilon, ilat
+    return ilon
 
 
-import PlasmaBubbles as pb 
 
 
-def plot_epb_drift(time, term, Dt, v0 = 100, x0 = -60):
+def growth_phase(
+        ax, 
+        epb_lon, 
+        count, 
+        max_edge = 10
+        ):
     
-    fig, ax = mappping(start_day.year)
+    if count <= max_edge: 
 
-    ax.set(title = time.strftime("%H:%M:%S (UT)"))
-
-    ilon, ilat = plot_terminator_line(ax, time)
-
-    epb_lon = displacement(x0, velocity(v0), Dt)
-
-    if (time >= term) and (epb_lon > ilon):
-
-        plot_ellipse(ax, lon = epb_lon)
-            
-    return fig
-
-from tqdm import tqdm 
-
-Dt = 2
-
+        plot_ellipse(
+                 ax, lon = epb_lon, 
+                 semi_major=count)
+        
+    else:
+        
+        plot_ellipse(
+                 ax, lon = epb_lon, 
+                 semi_major=max_edge)
 year = 2013
-start_day = dt.datetime(year, 1, 1, 21)
+start = dt.datetime(year, 1, 1, 23, 0)
 
 b.make_dir('temp')
 
 def run(start_day, v0 = 100, x0 = -60):
     
     term = pb.terminator(x0, start_day, float_fmt = False) 
+    count = 0
+    for i, Dt in enumerate(np.arange(0, 2, 0.02)):
     
-    for Dt in tqdm(np.arange(0, 7, 0.02), 'save frames'):
-    
-        plt.ioff()
+        # plt.ioff()
         
         delta = dt.timedelta(hours = Dt)
         
         time = start_day + delta
-       
-        fig = plot_epb_drift(time, term, Dt)
+        
+        fig, ax = mappping(time)
+
+        
+                
+        
+            
+        
+        # fig = plot_epb_drift(time, term, Dt)
         
         name = time.strftime('%Y%m%d%H%M')
+        
+        plt.show()
        
-        fig.savefig(f'temp/{name}')
-            
-        plt.clf()   
-        plt.close()
+        # fig.savefig(f'temp/{name}')
+        
+        # plt.clf()   
+        # plt.close()
 
 # delta = dt.timedelta(hours = 3)
 # time = start_day + delta
 # fig = plot_epb_drift(time, start_day, v0 = 100, x0 = -60)
 
-# plt.show()
-v0 = 100
+# run(start_day)
+
+
+
 x0 = -60
-run(start_day)
+term = pb.terminator(x0, start, float_fmt = False) 
+v0 = 100
+Dt = 1
+
+def update(start, Dt):
+    delta = dt.timedelta(hours = Dt)
+    
+    return start + delta
+    
+
+count = 0
+for i, Dt in enumerate(np.arange(0, 2, 0.1)):
+      
+    time = update(start, Dt)
+    
+    fig, ax = mappping(time)
+    
+    ilon = plot_terminator_line(time)
+    
+    epb_lon = displacement(x0, velocity(v0), Dt)
+    # if (time >= term):
+    if (epb_lon > ilon):
+        count += 1 
+        growth_phase(ax, epb_lon, count)
+   
+            
+            
+            
+
+
+plt.show()
